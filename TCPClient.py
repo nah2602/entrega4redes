787a7a6fe49cd12174920c8bcffc8da637798f8a
nah2602 <83468573+nah2602@users.noreply.github.com>
2023-04-26 11:25:36 -0300

    Create TCPClient.py

---
+++ TCPClient.py
@@ -0,0 +1,20 @@
+from socket import *
+
+nome_host = '127.0.0.1'
+porta = 12000
+
+client = socket(AF_INET, SOCK_STREAM)
+client.connect((nome_host, porta))
+
+req = 'GET / HTTP/1.1\nHost: ' + nome_host + '\n\n'
+client.send(req.encode())
+
+res = client.recv(4096).decode()
+
+header, body = res.split('\n\n', 1)
+if header.split()[1] == '200':
+    print(body)
+else:
+    print(' Erro')
+
+client.close()
